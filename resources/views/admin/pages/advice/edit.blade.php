0224460d251b69619646d0d37e42d92ace477618
Alaa Ali <alaa.saadallah96@gmail.com>
2024-05-12 04:45:19 +0300

    advice crud

--- resources/views/admin/pages/advice/edit.blade.php
+++ resources/views/admin/pages/advice/edit.blade.php
@@ -1,39 +1,65 @@
-@extends('admin.layouts._app')
+@extends('admin.layout.master')
 
+@section('title', __('dashboard.advice-edit'))
 
-@section('title', __('dashboard.city-create'))
+@push('plugin-styles')
+<link href="{{ asset('assets/plugins/datatables-net-bs5/dataTables.bootstrap5.css') }}" rel="stylesheet" />
+@endpush
 
-@section('main')
-    <form action="{{ route('admins.advices.update', $advice->id) }}" method="post">
-        @csrf @method('PATCH')
+@section('content')
+<nav class="page-breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="{{route ('admins.advices.index') }}">{{ __('dashboard.advice-index')}} </a></li>
+    <li class="breadcrumb-item active" aria-current="page">{{ __('dashboard.advice-edit')}}</li>
+  </ol>
+</nav>
+@include('admin.layout.flash')
 
-        <label for="title_en">{{ __('dashboard.title-advice-en') }}</label>
-        <input type="text" name="title[en]" id="title_en" value="{{ $advice->getTranslation('title', 'en')}}" @class(['form-control', 'is-invalid' => $errors->has('title.en')])>
+<form class="forms-sample" action="{{ route('admins.advices.update', $advice->id) }}" method="post" enctype="multipart/form-data">
+  @csrf
+  @method('PATCH')
+  <div class="row mb-3">
+    <div class="col-lg-3">
+      <label for="defaultconfig-3" class="col-form-label">Image</label>
+    </div>
+    <div class="col-lg-8">
+      <img src="{{asset($advice->img)}}" class="w-50">
+      <input type="file" id="img" name="img" value="{{ old('img') }}" class="form-control">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="exampleInputUsername2" class="col-sm-3 col-form-label">{{ __('dashboard.title-advice-en') }}</label>
+    <div class="col-sm-9">
+      <input type="text" class="form-control" name="title[en]" id="title_en" value="{{ $advice->getTranslation('title', 'en')}}">
 
-        <label for="title_ar">{{ __('dashboard.title-advice-ar') }}</label>
-        <input type="text" name="title[ar]" id="title_ar" value="{{ $advice->getTranslation('title', 'ar')}}" @class(['form-control', 'is-invalid' => $errors->has('title.ar')])>
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="exampleInputEmail2" class="col-sm-3 col-form-label">{{ __('dashboard.title-advice-ar') }}</label>
+    <div class="col-sm-9">
+      <input type="text" class="form-control" name="title[ar]" id="title_ar" value="{{ $advice->getTranslation('title', 'ar')}}">
+    </div>
+  </div>
 
-        <textarea name="description[en]" id="" @class([
-            'form-control',
-            'is-invalid' => $errors->has('description.en'),
-        ]) cols="30" rows="10">
-            {{ $advice->getTranslation('description', 'en') }}
-        </textarea>
+  <div class="row mb-3">
+    <div class="col-lg-3">
+      <label for="defaultconfig" class="col-form-label">Description (EN)</label>
+    </div>
+    <div class="col-lg-8">
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" name="description[en]">{{ $advice->getTranslation('description', 'en')}}</textarea>
+    </div>
+  </div>
 
-        <textarea name="description[ar]" id="" @class([
-            'form-control',
-            'is-invalid' => $errors->has('description.ar'),
-        ]) cols="30" rows="10">
-            {{ $advice->getTranslation('description', 'ar') }}
-        </textarea>
-
-
-        <img src="{{asset($advice->img)}}" alt="Medical Advice" srcset=""> 
-        <input type="file" name="img" id="">
-        <button type="submit">{{ __('dashboard.submit') }}</button>
-    </form>
-
-    @if ($errors->any())
-        {{ implode('', $errors->all('<div>:message</div>')) }}
-    @endif
+  <div class="row mb-3">
+    <div class="col-lg-3">
+      <label for="defaultconfig" class="col-form-label">Description (AR)</label>
+    </div>
+    <div class="col-lg-8">
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" name="description[ar]">{{ $advice->getTranslation('description', 'en')}}</textarea>
+    </div>
+  </div>
+  
+  <button type="submit" class="btn btn-primary me-2">Submit</button>
+  <button class="btn btn-secondary">Cancel</button>
+</form>
 @endsection
