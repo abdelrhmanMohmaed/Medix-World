68b65cfa19aa2cfd0c54362c3e38a2138953e567
Abdelrahman Mohamed <76571972+abdelrhmanMohmaed@users.noreply.github.com>
2024-05-14 00:07:51 +0300

    Push Fix error

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,41 +23,5 @@ class User extends Authenticatable
 
     protected $casts = ['email_verified_at' => 'datetime','password' => 'hashed'];
 
-    public function setDateOfBirthAttribute($value)
-    {
-        $this->attributes['dateOfBirth'] = Carbon::parse($value)->format('Y-m-d');
-    }
-    public function scopeActive($query)
-    {
-        return $query->where('active', true);
-    }
-    // public function serviceProviderDetails() : HasMany
-    // {
-    //     return $this->hasMany(ServiceProviderDetails::class);
-    // }
-    public function phones() : HasMany
-    {
-        return $this->hasMany(Phone::class);
-    }
-    public function serviceProviderSchedule() : HasMany
-    {
-        return $this->hasMany(Schedule::class);
-    }    
-    public function book() : HasMany
-    {
-        return $this->hasMany(Book::class);
-    }   
-    public function clientBook() : HasMany
-    {
-        return $this->hasMany(Book::class,'client_id');
-    }    
-    public function review() : HasMany
-    {
-        return $this->hasMany(Review::class);
-    }   
-    public function clientReview() : HasMany
-    {
-        return $this->hasMany(Review::class,'client_id');
-    }
-
+    
 }
