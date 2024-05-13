8925a34cbb4b04180682b499a1aff045bb473ad7
Abdelrahman Mohamed <76571972+abdelrhmanMohmaed@users.noreply.github.com>
2024-05-14 00:17:55 +0300

    Last Updated From A.M 13-05-2024
    1- Update a background in (website and color and background color).
    2- Fix a search model design.
    3- Add a feature as an anchor to return to the first section on the page.
    4- Add a feature search model included in all pages on the website.
    5- Make separate CSS and JS files.
    6- Add a feature to display service providers' details (rating, comments, schedules).
    7- Add a feature Booking Appointment.
    8- Add a feature to create a service provider schedule  (In progress).
    9- Updated a feature in the design of the service provider search result page (In progress).
    10- Add a feature to create a service provider schedule  (In progress).

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,5 +16,49 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+   
+    protected $fillable = [
+        'name', 'email', 'password', 'dateOfBirth', 'gender', 'active'
+    ];
+    protected $hidden = [ 'password', 'remember_token'];
+
+    protected $casts = ['email_verified_at' => 'datetime','password' => 'hashed'];
     
+    public function setDateOfBirthAttribute($value)
+    {
+        $this->attributes['dateOfBirth'] = Carbon::parse($value)->format('Y-m-d');
+    }
+    public function scopeActive($query)
+    {
+        return $query->where('active', true);
+    }
+    public function serviceProviderDetails() : HasMany
+    {
+        return $this->hasMany(ServiceProviderDetails::class);
+    }
+    public function phones() : HasMany
+    {
+        return $this->hasMany(Phone::class);
+    }
+    public function serviceProviderSchedule() : HasMany
+    {
+        return $this->hasMany(Schedule::class);
+    }    
+    public function book() : HasMany
+    {
+        return $this->hasMany(Book::class);
+    }   
+    public function clientBook() : HasMany
+    {
+        return $this->hasMany(Book::class,'client_id');
+    }    
+    public function review() : HasMany
+    {
+        return $this->hasMany(Review::class);
+    }   
+    public function clientReview() : HasMany
+    {
+        return $this->hasMany(Review::class,'client_id');
+    }
+ 
 }
